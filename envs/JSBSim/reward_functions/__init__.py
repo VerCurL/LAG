865de2f83845cd44943eee0efb3d50c1c78b5081
VerCurL <fengkr_010628@163.com>
2025-09-08 17:09:48 +0800

    policy2v2

--- envs/JSBSim/reward_functions/__init__.py
+++ envs/JSBSim/reward_functions/__init__.py
@@ -3,5 +3,5 @@ from .backup.heading_reward import HeadingReward
 from .backup.shoot_penalty_reward import ShootPenaltyReward
 from .backup.posture_reward import PostureReward
 
-from .v1 import (AltitudeReward, MissilePostureReward, EventDrivenReward,
+from .v2 import (AltitudeReward, MissilePostureReward, EventDrivenReward,
                  TeamPostureReward, TeamAttackDefenseReward, MissileAvoidReward)
