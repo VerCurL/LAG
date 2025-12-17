ac5ed03cc1f0044da7a073ed72b474238bed32e2
VerCurL <fengkr_010628@163.com>
2025-12-17 12:24:34 +0800

    MAPPO algorithm

--- scripts/train/train_win/train_jsbsim_win_multi.py
+++ scripts/train/train_win/train_jsbsim_win_multi.py
@@ -179,6 +179,5 @@ if __name__ == "__main__":
         '--recurrent-hidden-size', '128',
         '--recurrent-hidden-layers', '1',
         '--data-chunk-length', '8',
-        '--user-name', 'fkr',
-        '--use-prior'
+        '--user-name', 'fkr'
     ])
